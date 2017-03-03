55e6bc8f1b3ac42ef2286a209fae149eb5f16546
ZayyadGaya <bzaydgaya@gmail.com>
2017-03-03 13:29:21 +0000

    Added client project

---
+++ client/src/main/java/uk/ac/ncl/csc8109/team1/client/ClientApp.java
@@ -0,0 +1,15 @@
+package uk.ac.ncl.csc8109.team1.client;
+
+import uk.ac.ncl.csc8109.team1.crypto.CryptoInterface;
+import uk.ac.ncl.csc8109.team1.crypto.Crypto;
+/**
+ * 
+ *
+ */
+public class ClientApp 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
