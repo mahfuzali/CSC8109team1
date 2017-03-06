bc95220db78b014aad1d74aa812496b9b57014f6
ZayyadGaya <bzaydgaya@gmail.com>
2017-03-06 14:57:10 +0000

    client project

---
+++ client/src/main/java/uk/ac/ncl/csc8109/team1/client/Source.java
@@ -0,0 +1,29 @@
+package uk.ac.ncl.csc8109.team1.client;
+
+public class Source {
+	//**Starts exchange**//
+	public static void main(String[] args) {
+		Client source  = new Client();
+		
+	
+	
+	//**Request exchange from TDS, which is label(id) of exchange**//
+	
+	
+	//**Wait for exchange id from TDS**//
+	
+	
+	//**Send message1 to TDS
+	//doc along with signature of A containing hash of doc (doc, sigA(h(doc)))**//
+	
+	
+	//**wait for message(Evidence of origin) from TDS at the of exchange
+	//which is signature of B containing signature of A which contains hash of the doc(sigB(sigA(h(doc)) **//
+	
+	
+	//**Output then end**//
+		
+	}
+	
+
+}
