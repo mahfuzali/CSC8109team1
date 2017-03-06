e058a081d2ccc93c90cf4980cbf53cffe0016d59
ZayyadGaya <bzaydgaya@gmail.com>
2017-03-06 15:39:17 +0000

    client project

--- client/src/main/java/uk/ac/ncl/csc8109/team1/client/Source.java
+++ client/src/main/java/uk/ac/ncl/csc8109/team1/client/Source.java
@@ -1,5 +1,10 @@
 package uk.ac.ncl.csc8109.team1.client;
 
+import java.io.File;
+
+import uk.ac.ncl.csc8109.team1.crypto.Crypto;
+import uk.ac.ncl.csc8109.team1.crypto.CryptoInterface;
+
 public class Source {
 	//**Starts exchange**//
 	public static void main(String[] args) {
@@ -15,7 +20,16 @@ public class Source {
 	
 	//**Send message1 to TDS
 	//doc along with signature of A containing hash of doc (doc, sigA(h(doc)))**//
+		File f = new File("src/main/java/sample");
 	
+		CryptoInterface a = new Crypto();
+		
+		String hash = a.getHashOfFile(f);
+		System.out.println(hash);
+		
+		//String signature = a.getSignature();
+		//System.out.println(sig);
+		
 	
 	//**wait for message(Evidence of origin) from TDS at the of exchange
 	//which is signature of B containing signature of A which contains hash of the doc(sigB(sigA(h(doc)) **//
