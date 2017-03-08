f032f94deb64ece8d5315b2cfa263d8ea1a014ef
Mahfuz Ali <m.ali4@newcastle.ac.uk>
2017-03-08 22:58:35 +0000

    "Changes made to interface"

--- core/src/main/java/uk/ac/ncl/csc8109/team1/crypto/CryptoInterface.java
+++ core/src/main/java/uk/ac/ncl/csc8109/team1/crypto/CryptoInterface.java
@@ -69,17 +69,15 @@ public interface CryptoInterface {
 	 */
 	String isVerified(String hash, String publicKey, String signature); 
 	
-	
 	/**
 	 * Stores the keypair in the local file system
 	 * @param <code>path<code> file to store the keypair
 	 */
-	public void storeKeyPair(String path);
-	
+	void storeKeyPair(String path);
 	
 	/**
 	 * Loads the keypair from a file 
 	 * @param <code>path<code> file where the keypair is stored
 	 */
-	public void loadKeyPair(String path);
+	void loadKeyPair(String path);
 }
