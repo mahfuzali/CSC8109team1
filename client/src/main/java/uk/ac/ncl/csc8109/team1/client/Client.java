2d6ab62188d6ed63289a1e070370ed9b9420ae19
Mahfuz Ali <m.ali4@newcastle.ac.uk>
2017-03-09 16:47:12 +0000

    Reading and writing to a file has changed

--- client/src/main/java/uk/ac/ncl/csc8109/team1/client/Client.java
+++ client/src/main/java/uk/ac/ncl/csc8109/team1/client/Client.java
@@ -1,3 +1,34 @@
+/*
+ * Copyright (c) Mahfuz Ali - Team 1 CSC8109. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   - Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   - Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *
+ *   - Neither the name of Oracle or the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 package uk.ac.ncl.csc8109.team1.client;
 
 import uk.ac.ncl.csc8109.team1.crypto.CryptoInterface;
@@ -5,14 +36,19 @@ import uk.ac.ncl.csc8109.team1.crypto.Crypto;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.UUID;
 
-
+/** 
+ * This class represents a client in a fair-exchange protocol
+ * 
+ * @author Mahfuz Ali
+ * @Version 1.3
+ * @email m.ali4@newcastle.ac.uk
+ */
 public class Client {
 	private String uid;
 	private String publicKey;
@@ -24,7 +60,9 @@ public class Client {
 	private String tds;
 	private String source;
 	
-	
+	/**
+	 * 
+	 */
 	public Client() {
 		uid = UUID.randomUUID().toString();
 		crypto = new Crypto();
@@ -33,84 +71,191 @@ public class Client {
 		this.queueName = UUID.randomUUID().toString() + "_queue";
 	}
 
+	/**
+	 * 
+	 * @return
+	 */
 	public String getUUID() {
 		return uid;
 	}
 
+	/**
+	 * 
+	 * @return
+	 */
 	public String getPublicKey() {
 		return publicKey;
 	}
 
+	/**
+	 * 
+	 * @return
+	 */
 	public String getPrivateKey() {
 		return privateKey;
 	}
 
+	/**
+	 * 
+	 * @return
+	 */
 	public String getQueueName() {
 		return queueName;
 	}
 
+	/**
+	 * 
+	 * @return
+	 */
 	public String getLabel() {
 		return label;
 	}
 
+	/**
+	 * 
+	 * @param label
+	 */
 	public void setLabel(String label) {
 		this.label = label;
 	}
 
+	/**
+	 * 
+	 * @return
+	 */
 	public String getTds() {
 		return tds;
 	}
 
+	/**
+	 * 
+	 * @param tds
+	 */
 	public void setTds(String tds) {
 		this.tds = tds;
 	}
 
+	/**
+	 * 
+	 * @return
+	 */
 	public String getSource() {
 		return source;
 	}
 
+	/**
+	 * 
+	 * @param source
+	 */
 	public void setSource(String source) {
 		this.source = source;
 	}
 	
-	
-	public void writeToFile(String filePath, String tds, String source, String label){
-		// Creating a new file
-	    Path newFile = Paths.get(filePath);
-	    try {
-	      Files.deleteIfExists(newFile);
-	      newFile = Files.createFile(newFile);
-	    } catch (IOException ex) {
-	      System.out.println("Error creating file");
-	    }
-	    System.out.println(Files.exists(newFile));
-
-	    //Writing to file
-	    try(BufferedWriter writer = Files.newBufferedWriter(
-	            newFile, Charset.defaultCharset())){
-	      writer.append(tds + "," +  source + "," + label);
-	      writer.newLine();
-	      writer.flush();
-	    } catch(IOException exception){
-	      System.out.println("Error writing to file");
-	    }
+	/**
+	 * 
+	 * @param FILENAME
+	 * @param data
+	 */
+	public void writeToFile(String FILENAME, String data) {
+		if(check(FILENAME, data) == false) {
+			write(FILENAME, data);
+		} else {
+			System.out.println("Data already exists");
+		}
 	}
 	
-	
-	public void readFromFile(String filePath){
-		//Reading from file
-	    Path newFile = Paths.get(filePath);
-	    try(BufferedReader reader = Files.newBufferedReader(
-	    		newFile, Charset.defaultCharset())){
-	      String lineFromFile = "";
-	      System.out.println("\nThe contents of file are: ");
-	      while((lineFromFile = reader.readLine()) != null){
-	        System.out.println(lineFromFile);
-	      }
-
-	    } catch(IOException exception){
-	      System.out.println("Error while reading file");
-	    }
-		
+	/**
+	 * 
+	 * @param FILENAME
+	 */
+	public void readFromFile(String FILENAME) {
+		BufferedReader br = null;
+		FileReader fr = null;
+		try {
+			String sCurrentLine;
+			br = new BufferedReader(new FileReader(FILENAME));
+			while ((sCurrentLine = br.readLine()) != null) {
+				System.out.println(sCurrentLine);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (br != null)
+					br.close();
+				if (fr != null)
+					fr.close();
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
 	}
+	
+	/**
+	 * 
+	 * @param FILENAME
+	 * @param data
+	 */
+	private void write(String FILENAME, String data) {
+		BufferedWriter bw = null;
+		FileWriter fw = null;
+		try {
+			File file = new File(FILENAME);
+			// if file doesnt exists, then create it
+			if (!file.exists())
+				file.createNewFile();
+			// true = append file
+			fw = new FileWriter(file.getAbsoluteFile(), true);
+			bw = new BufferedWriter(fw);
+			bw.write(data);
+			bw.newLine();
+			System.out.println("Done");
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (bw != null)
+					bw.close();
+				if (fw != null)
+					fw.close();
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}	
+	}
+	
+	/**
+	 * 
+	 * @param FILENAME
+	 * @param data
+	 * @return
+	 */
+	private boolean check(String FILENAME, String data) {
+		boolean flag = false;
+		BufferedReader br = null;
+		FileReader fr = null;		
+		try {
+			String sCurrentLine;
+			br = new BufferedReader(new FileReader(FILENAME));
+			while ((sCurrentLine = br.readLine()) != null) {
+				if(sCurrentLine.equals(data)) {
+					flag = true;
+					break;
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (br != null)
+					br.close();
+				if (fr != null)
+					fr.close();
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}			
+		return flag;
+	}
+
 }
