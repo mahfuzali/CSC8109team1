0fb397f98ce115a5e57f0cc71a89f35a60c73243
ZayyadGaya <bzaydgaya@gmail.com>
2017-03-10 13:13:25 +0000

    created Crypto test

---
+++ core/src/test/java/uk/ac/ncl/csc8109/team1/crypto/CryptoTest.java
@@ -0,0 +1,14 @@
+package uk.ac.ncl.csc8109.team1.crypto;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CryptoTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
