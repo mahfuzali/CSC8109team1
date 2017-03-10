f233b854b2c1528c5c09b4030ae08504f2b3e8ca
ZayyadGaya <bzaydgaya@gmail.com>
2017-03-10 13:55:36 +0000

    message test unit class

---
+++ core/src/test/java/uk/ac/ncl/csc8109/team1/message/MessageTest.java
@@ -0,0 +1,14 @@
+package uk.ac.ncl.csc8109.team1.message;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MessageTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
